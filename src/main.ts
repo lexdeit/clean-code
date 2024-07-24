13a0f3b13e42376d9baba521d5ed31f01a1d8d17
lexdeit <godhood@proton.me>
2024-07-24 17:49:46 -0600

    ci: prueba merge

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,8 @@ const app = document.querySelector<HTMLDivElement>('#app')!
 app.innerHTML = `
   <h1>CleanCode y SOLID</h1>
   <span>Revisar la consola de JavaScript</span>
+
+
+  ESTO ES UNA PRUEBA
 `
 
